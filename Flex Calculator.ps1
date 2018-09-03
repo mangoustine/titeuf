3610022077d5992b83fcf4ca97ead158b4342b4b
mangoustine <p.aubriel5@gmail.com>
2018-09-03 15:21:33 +0200

    ajout calc

---
+++ Flex Calculator.ps1
@@ -0,0 +1,31 @@
+﻿clear-host  
+Write-Host " MiniCalc V1.1 By Master Flex"
+
+# Demande le premier nombre à l'utilisateur
+[int] $input1 = read-host "First number"
+
+#Affiche les opérations disponibles
+echo "+--------------------------+"
+echo "| [ + ] : Add              |"
+echo "| [ - ] : Substract        |"
+echo "| [ * ] : Multiplicate     |"
+echo "| [ / ] : Divide           |"
+echo "+--------------------------+"
+
+#Demande l'opértion souhaitée par l'utilisateur
+[String] $op = Read-Host "operation"
+
+# Demade le deuxième nopbre par l'utilisateur
+[int] $input2 = Read-host "Second numero"
+
+#Effectue l'opération souhaitée en fonction de la saisie utilisateur
+switch ($op) {
+"+" { $res = $input1 + $input2 }
+"-" { $res = $input1 - $input2 }
+"*" { $res = $input1 * $input2 }
+"/" { $res = $input1 / $input2 }
+default { $res = "Error" }
+}
+
+# Affiche le résultat (`n représente un saut de ligne)
+write-host "`nResult >> $res`n`a"
